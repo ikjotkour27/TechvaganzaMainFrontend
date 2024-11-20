27f54f4acdf40c45000799f7153ca79c804d9c2a
ikjotkour27 <ikjotkourmultani.27@gmail.com>
2024-11-20 17:58:39 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: true, // or '0.0.0.0'
+  },
+  plugins: [react()],
+})
